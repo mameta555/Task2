8f09459addc07170c0fe7914b3ded882a403b80c
EC2 Default User <ec2-user@ip-172-31-15-158.ap-northeast-1.compute.internal>
2022-11-13 10:16:38 +0000

    [Finish]詳細画面編集機能のコントローラ

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  get 'books' => 'books#new'
+  post 'books' => 'books#create'
+  get 'books/:id' =>'books#show', as: 'book'
+  get 'books/:id/edit' => 'books#edit', as: 'edit_book'
+  patch 'books/:id' => 'books#update', as: 'update_book'
+  get '/top' => 'homes#top'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
